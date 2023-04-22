439e96500589c404fb5fa58fc888f1d4dbf97faf
Pikusev <pikusev.dima@mail.ru>
2023-04-22 16:19:55 +0400

    update models

--- CarsSite/CarsSite/models.py
+++ CarsSite/CarsSite/models.py
@@ -21,18 +21,14 @@ class Car(models.Model):
     Name = models.TextField(null=False)
     Cost = models.TextField(null=False)
     Speed = models.TextField(null=False)
-    Brand = models.ForeignKey(Brand,
-                                 on_delete=models.SET_NULL,
-                                 null=True
-                                 )
-    Rate = models.ForeignKey(Rate,
-                                 on_delete=models.SET_NULL,
-                                 null=True
-                                 )
-
+    BrandId = models.ForeignKey(Brand,
+                                on_delete=models.SET_NULL,
+                                null=False
+                                )
 
 
 class Rate(models.Model):
     id = models.AutoField(primary_key=True)
     Person = models.ForeignKey(Person, on_delete=models.CASCADE)
     Car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    Stars = models.CharField(null=True)
